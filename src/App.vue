41ac18f493e7bf2bbb9d758a6bebfeb750067dde
Uribe02 <axel02Uribe10@gmail.com>
2024-09-25 17:20:26 +0000

    ⭐fead: leable,item,items

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import{ref}from'vue';
 const header = ref ('app lista de compras');
+const items = ref([
+    {id:'0',label:'2 chetos Bolitas de queso'},
+    {id:'1',label:'1 bolsa nues'},
+    {id:'2',label:'3 palomitas'},
+    {id:'3',label:'1 nutella'},
+    
 
+]);
 </script>
 
 <template>
@@ -9,11 +16,15 @@ const header = ref ('app lista de compras');
 <h1>
     <i class="material-icons">local_mall</i>
     {{ header }}
-    </h1>
+</h1>
+<ul>
+    
+<li v-for="item in items" :key="item.id">➡️ {{ item.label }}</li>
+</ul>
 </template>
 
 <style scoped>
-.shopping-cart-icon{
+.shopping-cart-icon{ 
     font-size:2rem ;
 }
 </style>
