cd5ed3e5f0c4ea272e9ed02b8bf32dae233e6a10
Uribe02 <axel02Uribe10@gmail.com>
2024-10-02 17:47:43 +0000

    ⭐FEAD metodos Create method to add item and clean input

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,60 @@
 <script setup>
-import{ref}from'vue';
-const header = ref ('app lista de compras');
+import { ref } from 'vue';
+// Modelo
+const header = ref('App lista de compras');
 const items = ref([
-    {id:'0',label:'2 chetos Bolitas de queso'},
-    {id:'1',label:'1 bolsa nues'},
-    {id:'2',label:'3 palomitas'},
-    {id:'3',label:'1 nutella'},
-    
-
+  {id:'0', label: '10 bolillos'},
+  {id:'1', label: '1 crema de litro'},
+  {id:'2', label: '1/4 de jamon'},
+  {id:'3', label: '1 nutella'}
 ]);
-const newItem = ref('');
-const newItemPriority=ref('low');
+const newItem = ref("");
 const newItemHighPriority = ref(false);
 const iceCreamFlavors = ref([]);
 </script>
 
 <template>
-
-<h1>
-    <i class="material-icons">local_mall</i>
-    {{ header }}
-</h1>
-<input v-model.lazy="newItem" type="tex"
- placeholder="Agregar articulo">
- <!-- Radio de botones-->
+  <h1>
+   <i class="material-icons shopping-cart-icon">local_mall</i> 
+    {{ header }} 
+  </h1>
+  <form v-on:submit.prevent="items.push({id: items.length + 1, label: newItem})" class="add-item form">
+  <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
+  <!--Caja de seleccion de Prioridad-->
   <label>
-    <input type="radio" value="low" v-model="newItemPriority">
-    Baja
-</label>
-  <label>
-    <input type="radio" value="high" v-model="newItemPriority">
-    Alta
-</label>
-{{ newItemPriority =='low'?'🦤':'🧸'}}
+    <input type="checkbox" v-model="newItemHighPriority" />
+    Alta Prioridad
+  </label>
+  <!--Boton-->
+  <button class="btn btn-primary">
+    Salvar Articulo
+  </button>
+  </form>
+  <ul></ul>
   <!-- Helados -->
+   <label>
+    <input type="checkbox" v-model="iceCreamFlavors" value="Vanilla" />
+    Vanilla
+  </label>
   <label>
-		<input type="checkbox" v-model="iceCreamFlavors" value="vanilla">
-		Vanilla
-	</label>
+    <input type="checkbox" v-model="iceCreamFlavors" value="Chocolate" />
+    Chocolate
+  </label>
   <label>
-		<input type="checkbox" v-model="iceCreamFlavors" value="chocolate">
-		Chocolate
-	</label>
-  <label>
-		<input type="checkbox" v-model="iceCreamFlavors" value="strawnerry">
-		Strawberry
-	</label>
-<ul>
-    
-<li v-for="item in items" :key="item.id">➡️ {{ item.label }}</li>
-</ul>
+    <input type="checkbox" v-model="iceCreamFlavors" value="Cookie and cream" />
+    Cookie and cream
+  </label>
+  {{ iceCreamFlavors }}
+  <ul></ul>
+  {{ newItemHighPriority }}
+  <!-- Lista -->
+  <ul>
+    <li v-for="item in items" :key="item.id"> 🛍 {{  item.label }} </li>
+  </ul>
 </template>
 
 <style scoped>
-.shopping-cart-icon{ 
-    font-size:2rem ;
+.shopping-cart-icon{
+  font-size: 2rem;
 }
 </style>
