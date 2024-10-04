41579f3ec77ac8122659bc40add990657b695e28
Uribe02 <axel02Uribe10@gmail.com>
2024-10-04 16:54:06 +0000

    ✨app colocando iperenlace

--- src/App.vue
+++ src/App.vue
@@ -17,19 +17,40 @@ const seveItem = () => {
   //limpia el imput 
   newItem.value='';
 };
+//---formulario--
 const newItem = ref('');
 const newItemHighPriority = ref(false);
+const editing = ref(true);
+const activateEdition = (activate)=>{
+  editing.value=activate;
+};
 
 //metodos 
 </script>
 
 <template>
+  
+<div class="header">
   <h1>
    <i class="material-icons shopping-cart-icon">local_mall</i> 
     {{ header }} 
   </h1>
+  <button
+   v-if="editing" 
+   class="btn"
+    @click="activateEdition(false)">
+    Cancelar
+  </button>
+  <button
+   v-else
+    class="btn btn-primary" 
+    @click="activateEdition(true)">
+    Agregar Articulo
+  </button>
+  </div>
   <form 
   class="add-item form"
+  v-if="editing"
    v-on:submit.prevent="seveItem">
   <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
   <!--Caja de seleccion de Prioridad-->
@@ -51,6 +72,7 @@ const newItemHighPriority = ref(false);
   <ul>
     <li v-for="item in items" :key="item.id"> 🛍 {{  item.label }} </li>
   </ul>
+  <p V-if="items.length === 0">🥀NO HAY ELEMNTOS EN LISTA🥀 </p>
 </template>
 
 <style scoped>
